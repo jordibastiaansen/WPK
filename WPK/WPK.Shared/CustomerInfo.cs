47798247ba0f674b7901215b8f8fae049571a58b
Davilicus <florisslenderman@gmail.com>
2017-03-13 09:41:42 +0100

    Toevoegen main scherm
    Alleen even de versie met main scherm toegevoegd. Rest is nog steeds WIP

---
+++ WPK/WPK.Shared/CustomerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPK
+{
+    public class CustomerInfo
+    {
+        public string name { get; set; }
+        public string adress { get; set; }
+        public string telephone { get; set; }
+        //public string extra { get; set; }
+    }
+}
