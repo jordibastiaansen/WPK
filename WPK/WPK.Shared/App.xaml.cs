3cc1fd571ba26701d74ccd4441de05caa34bd841
Davilicus <florisslenderman@gmail.com>
2017-03-15 09:43:57 +0100

    Back buton fix
    Terugknop werkt nu normaal, in plaats van dat hij de app sluit.

--- WPK/WPK.Shared/App.xaml.cs
+++ WPK/WPK.Shared/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Phone.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -37,6 +38,18 @@ namespace WPK
         {
             this.InitializeComponent();
             this.Suspending += this.OnSuspending;
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame != null && rootFrame.CanGoBack)
+            {
+                e.Handled = true;
+                rootFrame.GoBack();
+            }
         }
 
         /// <summary>
