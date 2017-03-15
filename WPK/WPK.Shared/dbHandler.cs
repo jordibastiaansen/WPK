09ebc77de5149b07f490924cf780183584b082bf
Davilicus <florisslenderman@gmail.com>
2017-03-15 08:48:20 +0100

    Blep
    Heb blijkbaar veranderingen

--- WPK/WPK.Shared/dbHandler.cs
+++ WPK/WPK.Shared/dbHandler.cs
@@ -31,8 +31,8 @@ namespace WPK
             list[2] = new CustomerInfo() { adress = "Schoutstraat 14a, 4204BC, Gorinchem, Nederland", name = "Ijsco", telephone = "0275-576643" };
             list[3] = new CustomerInfo() { adress = "Schoutstraat 16a, 4204BC, Gorinchem, Nederland", name = "Flem", telephone = "0275-576643" };
             list[4] = new CustomerInfo() { adress = "Schoutstraat 18a, 4204BC, Gorinchem, Nederland", name = "Verschnikket", telephone = "0275-576643" };
-            list[5] = new CustomerInfo() { adress = "Schoutstraat 20a, 4204BC, Gorinchem, Nederland", name = "Textbedrijf", telephone = "0275-576643" };
-            list[6] = new CustomerInfo() { adress = "Schoutstraat 11a, 4204BC, Gorinchem, Nederland", name = "Woop", telephone = "0275-576643" };
+            list[5] = new CustomerInfo() { adress = "Schoutstraat 20a, 4204BC, Gorinchem, Nederland", name = "Donald Trump", telephone = "0275-576643" };
+            list[6] = new CustomerInfo() { adress = "Schoutstraat 11a, 4204BC, Gorinchem, Nederland", name = "Kakkerlak", telephone = "0275-576643" };
             list[7] = new CustomerInfo() { adress = "Schoutstraat 13a, 4204BC, Gorinchem, Nederland", name = "Wagarble", telephone = "0275-576643" };
             list[8] = new CustomerInfo() { adress = "Schoutstraat 15a, 4204BC, Gorinchem, Nederland", name = "Flarf", telephone = "0275-576643" };
             list[9] = new CustomerInfo() { adress = "Schoutstraat 17a, 4204BC, Gorinchem, Nederland", name = "Myar", telephone = "0275-576643" };
